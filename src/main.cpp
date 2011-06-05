c431016d2c076974f7f9816000359b09e58e0fb4
Korrigan <korrigan@epitech.eu>
2011-06-05 18:07:17 +0200

    added main function

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+//
+// main.cpp for gameoflife in /home/korrigan/dev/proj/gameoflife
+// 
+// Made by Matthieu Rosinski
+// Login   <korrigan@epitech.net>
+// 
+// Started on  Sun Jun  5 13:01:25 2011 Matthieu Rosinski
+// Last update Sun Jun  5 13:01:25 2011 Matthieu Rosinski
+//
+
+#include	<iostream>
+#include	<vector>
+#include	<cstdlib>
+
+#include	"GameOfLife.hpp"
+
+int		main(int ac, char **av)
+{
+  GameOfLife	engine;
+
+  if (ac == 2)
+    engine = GameOfLife(atoi(av[1]));
+  else
+    engine = GameOfLife();
+  while (engine.prepare())
+    {
+      engine.process();
+      engine.show();
+    }
+  return (0);
+}
